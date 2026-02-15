cd9bf47d0b731e2685593a5e667c25a53c0185a0
zinshin36 <leewd52@gmail.com>
2026-02-14 20:25:17 -0500

    Delete dps_formula.py

--- dps_formula.py
+++
@@ -1,7 +0,0 @@
-def calculate_dps(stats, base_int=500):
-    crit = stats.get("Crit",0)
-    dh = stats.get("DirectHit",0)
-    det = stats.get("Determination",0)
-    spd = stats.get("SpellSpeed",0)
-    # Simplified placeholder FFXIV formula
-    return base_int * (1 + crit/100) * (1 + dh/100) * (1 + det/100)
