f1d17b3caff53f9b247a59ad07b399ae585d0419
zinshin36 <leewd52@gmail.com>
2026-02-17 20:57:39 -0500

    Update data_models.py

--- data_models.py
+++ data_models.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 
 @dataclass
-class CharacterStats:
-    main_stat: int
-    crit: int
-    direct_hit: int
-    determination: int
-    skill_speed: int
-    weapon_damage: int
+class Spell:
+    name: str
+    potency: int
+    mp_cost: int
+    cast_time: float
+
+@dataclass
+class PlayerState:
+    mp: int = 10000
+    in_astral_fire: bool = False
+    in_umbral_ice: bool = True
+    time_elapsed: float = 0.0
